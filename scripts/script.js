7ae78afa3c8ce1f20c7f49ea01dfd9ab6fa0401c
Akos Murati <akos@murati.hu>
2019-02-14 19:26:57 +1300

    Clean script

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-/*-----------------------------------------------------------------------------------
-/*
-/* Script for Resume
-/*
------------------------------------------------------------------------------------*/
-
 /*----------------------------------------------------*/
 /* Preloader
 ------------------------------------------------------ */
@@ -199,65 +193,6 @@ function parallax() {
     });
      
     });
-
-
-/*----------------------------------------------------*/
-/*  Google Map
-------------------------------------------------------*/
-
-    // main directions
-      //map = new GMaps({
-      //  el: '#map', lat: 23.790223, lng: 90.414036, zoom: 13, zoomControl : true, 
-      //  zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
-      //});
-    // add address markers
-    //map.addMarker({ lat: 23.790223, lng: 90.414036, title: 'BD InfoSys',
-      //infoWindow: { content: '<p>Building # 2, Plot # 111, Road # 35, Gulshan - 2, Dhaka</p>' } });
-
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
-
-   $('form#contactForm button.submit').click(function() {
-
-      $('#image-loader').fadeIn();
-
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
-
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });
-      return false;
-   });
-
-
 });
 
 
